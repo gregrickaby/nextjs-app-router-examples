d708762393c474df8926872abd2e5090243ef124
Greg Rickaby <gregrickaby@gmail.com>
2023-02-17 12:18:08 -0600

    add default metadata. add comments.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,25 @@
+import Nav from "@/components/Navigation";
+import type { Metadata } from "next";
 import "./globals.css";
 
+/**
+ * Default metadata.
+ *
+ * @see https://beta.nextjs.org/docs/api-reference/metadata
+ */
+export const metadata: Metadata = {
+  title: {
+    default: "Home",
+    template: "%s | Next.js App Directory Sandbox",
+  },
+  description: "Welcome to the Next.js App Directory Sandbox",
+};
+
+/**
+ * The root layout.
+ *
+ * @see https://beta.nextjs.org/docs/api-reference/file-conventions/layout
+ */
 export default function RootLayout({
   children,
 }: {
@@ -9,6 +29,9 @@ export default function RootLayout({
     <html lang="en">
       <head />
       <body>
+        <header>
+          <Nav />
+        </header>
         <main className="container">{children}</main>
       </body>
     </html>
