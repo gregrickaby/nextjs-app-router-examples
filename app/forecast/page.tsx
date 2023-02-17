9307176643bdbb2c8869286b8769e88f06a96316
Greg Rickaby <gregrickaby@gmail.com>
2023-02-17 12:18:37 -0600

    update metadata. add comments.

--- app/forecast/page.tsx
+++ app/forecast/page.tsx
@@ -1,12 +1,18 @@
 import type { Metadata } from "next";
 
 export const runtime = "experimental-edge";
-
 export const metadata: Metadata = {
   title: "Forecast",
-  description: "Welcome to Next.js",
+  description: "The local forecast",
 };
 
+/**
+ * Fetch the initial forecast data.
+ *
+ * @see https://beta.nextjs.org/docs/data-fetching/fetching
+ *
+ * Note: This fetch happens on the server.
+ */
 async function getCurrentTemp() {
   const response = await fetch(
     `https://api.open-meteo.com/v1/gfs?latitude=31.32&longitude=-85.86&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&forecast_days=1&timezone=America%2FChicago`
@@ -19,8 +25,15 @@ async function getCurrentTemp() {
   return response.json();
 }
 
+/**
+ * The forecast page.
+ *
+ * @see https://beta.nextjs.org/docs/routing/pages-and-layouts
+ */
 export default async function Forecast() {
+  // Fetch the forecast.
   const temp = await getCurrentTemp();
+
   return (
     <>
       <h1>Forecast</h1>
