2c3b31d5697d5836233ff8897b5938680b3a27a1
Greg Rickaby <gregrickaby@gmail.com>
2023-02-21 14:57:02 -0600

    move current conditions to the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,48 @@
-// https://beta.nextjs.org/docs/rendering/edge-and-nodejs-runtimes#edge-runtime
+import WeatherTable from "@/components/WeatherTable";
+import config from "@/lib/config";
+import type { Metadata } from "next";
+import { Suspense } from "react";
+
 export const runtime = "experimental-edge";
+export const metadata: Metadata = {
+  title: config.siteName,
+  description: config.siteDescription,
+};
 
 /**
- * The homepage.
+ * Fetch the initial current conditions data.
  *
- * @see https://beta.nextjs.org/docs/routing/pages-and-layouts#pages
+ * @see https://beta.nextjs.org/docs/data-fetching/fetching
+ *
+ * Note: This fetch happens on the server.
  */
-export default function Homepage() {
+async function getCurrentConditions() {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=31.3151708&lon=-85.85521609999999&units=imperial&exclude=minutely&appid=${process.env.OPENWEATHER_API_KEY}`
+  );
+
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+
+  return response.json();
+}
+
+/**
+ * The Homepage.
+ *
+ * @see https://beta.nextjs.org/docs/routing/pages-and-layouts
+ */
+export default async function Homepage() {
+  const conditions = await getCurrentConditions();
+
   return (
     <>
-      <h1>Weather ⛈️</h1>
-      <p>
-        Welcome to the Next.js App Directory Sandbox. Click the links above to
-        play around.
-      </p>
+      <h1>Current Conditions</h1>
+      <Suspense fallback={<div>Loading current conditions...</div>}>
+        <h2>Enterprise, AL 36330, USA</h2>
+        <WeatherTable weather={conditions} />
+      </Suspense>
       <small>server component. edge runtime. streaming render.</small>
     </>
   );
