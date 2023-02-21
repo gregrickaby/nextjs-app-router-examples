87b5f1d2fbbb6baaa36f729a8208235390952fe6
Greg Rickaby <gregrickaby@gmail.com>
2023-02-21 14:57:14 -0600

    create a site config

---
+++ lib/config.ts
@@ -0,0 +1,23 @@
+const config = {
+  siteName: "Weather App",
+  siteDescription: "A simple weather app",
+  siteCredits: "© 2023 Weather App. All rights reserved.",
+  nav: [
+    {
+      label: "Home",
+      url: "/",
+      icon: "",
+    },
+    {
+      label: "About",
+      url: "/about",
+      icon: "",
+    },
+    {
+      label: "Search",
+      url: "/search",
+    },
+  ],
+};
+
+export default config;
