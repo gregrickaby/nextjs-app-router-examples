3eecb0415443aec1c02f7b3090ba8f7b0e70befe
Greg Rickaby <gregrickaby@gmail.com>
2023-02-25 17:15:35 -0600

    refactor weather table

--- components/WeatherTable.tsx
+++ components/WeatherTable.tsx
@@ -1,4 +1,6 @@
+import { formatDate, formatTemp, getTempColor } from "@/lib/functions";
 import { WeatherResponse } from "@/lib/types";
+import styles from "./WeatherTable.module.css";
 
 export default function WeatherTable({
   weather,
@@ -6,28 +8,38 @@ export default function WeatherTable({
   weather: WeatherResponse;
 }) {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Conditions</th>
-          <th>Temp</th>
-          <th>Wind</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>{weather?.current?.weather[0]?.main}</td>
-          <td>{weather?.current?.temp} F</td>
-          <td>{weather?.current?.wind_speed} mph</td>
-        </tr>
-      </tbody>
-      <tfoot>
-        <tr>
-          <td colSpan={3}>
-            <a href="https://openweathermap.org">Powered by OpenWeatherMap</a>
-          </td>
-        </tr>
-      </tfoot>
-    </table>
+    <section
+      className={styles.container}
+      style={
+        {
+          "--minTemp": getTempColor(weather.daily[0].temp.min),
+          "--maxTemp": getTempColor(weather.daily[0].temp.max),
+          "--currentTemp": getTempColor(weather.current.temp),
+          "--feelsLikeTemp": getTempColor(weather.current.feels_like),
+        } as React.CSSProperties
+      }
+    >
+      <time className={styles.time}>
+        {formatDate(weather?.current?.dt, weather?.timezone)}
+      </time>
+
+      <div className={styles.tempContainer}>
+        <div className={styles.tempRange}>
+          <span className={styles.minTemp}>
+            {formatTemp(weather?.daily[0]?.temp?.min)}
+          </span>
+          -
+          <span className={styles.maxTemp}>
+            {formatTemp(weather?.daily[0]?.temp?.max)}
+          </span>
+        </div>
+        <span className={styles.currentTemp}>
+          {formatTemp(weather?.current?.temp)}
+        </span>
+        <span className={styles.feelsLikeTemp}>
+          Feels Like {formatTemp(weather?.current?.feels_like)}
+        </span>
+      </div>
+    </section>
   );
 }
