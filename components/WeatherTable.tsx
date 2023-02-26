0e00a7ae6856092edeb232e578d6ab35d2bff0fe
Greg Rickaby <gregrickaby@gmail.com>
2023-02-25 21:56:30 -0600

    add more data. styles.

--- components/WeatherTable.tsx
+++ components/WeatherTable.tsx
@@ -1,4 +1,9 @@
-import { formatDate, formatTemp, getTempColor } from "@/lib/functions";
+import {
+  formatDate,
+  formatTemp,
+  getDirection,
+  getTempColor,
+} from "@/lib/functions";
 import { WeatherResponse } from "@/lib/types";
 import styles from "./WeatherTable.module.css";
 
@@ -12,10 +17,10 @@ export default function WeatherTable({
       className={styles.container}
       style={
         {
-          "--minTemp": getTempColor(weather.daily[0].temp.min),
-          "--maxTemp": getTempColor(weather.daily[0].temp.max),
-          "--currentTemp": getTempColor(weather.current.temp),
-          "--feelsLikeTemp": getTempColor(weather.current.feels_like),
+          "--minTemp": getTempColor(weather?.daily[0]?.temp?.min),
+          "--maxTemp": getTempColor(weather?.daily[0]?.temp?.max),
+          "--currentTemp": getTempColor(weather?.current?.temp),
+          "--feelsLikeTemp": getTempColor(weather?.current?.feels_like),
         } as React.CSSProperties
       }
     >
@@ -23,22 +28,47 @@ export default function WeatherTable({
         {formatDate(weather?.current?.dt, weather?.timezone)}
       </time>
 
-      <div className={styles.tempContainer}>
-        <div className={styles.tempRange}>
-          <span className={styles.minTemp}>
-            {formatTemp(weather?.daily[0]?.temp?.min)}
+      <div className={styles.row}>
+        <div className={styles.tempContainer}>
+          <div className={styles.tempRange}>
+            <span className={styles.minTemp}>
+              {formatTemp(weather?.daily[0]?.temp?.min)}
+            </span>
+            -
+            <span className={styles.maxTemp}>
+              {formatTemp(weather?.daily[0]?.temp?.max)}
+            </span>
+          </div>
+          <span className={styles.currentTemp}>
+            {formatTemp(weather?.current?.temp)}
           </span>
-          -
-          <span className={styles.maxTemp}>
-            {formatTemp(weather?.daily[0]?.temp?.max)}
+          <span className={styles.feelsLikeTemp}>
+            Feels Like {formatTemp(weather?.current?.feels_like)}
           </span>
         </div>
-        <span className={styles.currentTemp}>
-          {formatTemp(weather?.current?.temp)}
-        </span>
-        <span className={styles.feelsLikeTemp}>
-          Feels Like {formatTemp(weather?.current?.feels_like)}
-        </span>
+
+        <div className={styles.row}>
+          <div className={styles.column} style={{ textAlign: "right" }}>
+            <p>Sky</p>
+            <p>Wind</p>
+            <p>Pressure</p>
+            <p>Humidity</p>
+            <p>Dew Point</p>
+            <p>UV Index</p>
+          </div>
+
+          <div className={styles.column}>
+            <p>{weather?.current?.weather[0]?.main}</p>
+            <p>
+              {weather?.current?.wind_speed} mph{" "}
+              {getDirection(weather?.current?.wind_deg)}
+            </p>
+            <p>{weather?.current?.pressure} hPa</p>
+            <p>{weather?.current?.humidity}%</p>
+            <p>{formatTemp(weather?.current?.dew_point)}</p>
+            <p>{weather?.current?.uvi}</p>
+          </div>
+        </div>
       </div>
     </section>
   );
