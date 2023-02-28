b1d7d8b980ec6849df7c732f0854349acd9aef1f
Greg Rickaby <gregrickaby@gmail.com>
2023-02-28 07:26:02 -0600

    remove nav

--- lib/config.ts
+++ lib/config.ts
@@ -2,22 +2,7 @@ const config = {
   siteName: "Weather App",
   siteDescription: "A simple weather app",
   siteCredits: "© 2023 Weather App. All rights reserved.",
-  nav: [
-    {
-      label: "Current Conditions",
-      url: "/",
-      icon: "",
-    },
-    {
-      label: "About",
-      url: "/about",
-      icon: "",
-    },
-    {
-      label: "Search",
-      url: "/search",
-    },
-  ],
+  nav: [],
 };
 
 export default config;
