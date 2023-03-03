2bba4fbf8af5a91f9799328643f77589879c3cd3
Greg Rickaby <gregrickaby@gmail.com>
2023-03-03 13:31:14 -0600

    spiff up the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,52 @@ import Link from 'next/link'
 
 export default function Page() {
   return (
-    <div className="row items-center justify-center">
-      <h1>Choose your adventure...</h1>
-      <nav>
-        <Link className="button" href="/server/" prefetch={false}>
-          Server Components
-        </Link>{' '}
-        <Link className="button" href="/client/" prefetch={false}>
-          Client Components
-        </Link>
-      </nav>
+    <div className="column container">
+      <h1>Next.js App Directory</h1>
+
+      <div className="flex items-center gap-2">
+        <h2>Choose your adventure:</h2>
+        <nav>
+          <Link className="button" href="/server/" prefetch={false}>
+            Server
+          </Link>{' '}
+          or{' '}
+          <Link className="button" href="/client/" prefetch={false}>
+            Client
+          </Link>
+        </nav>
+      </div>
+
+      <h3>About</h3>
+      <p>
+        <strong>🔥 Server:</strong> Component hydration, rendering, <em>and</em>{' '}
+        data fetching occurs on the server. Then everything is streamed to your
+        browser. This is new with React 18 and Next.js 13.
+      </p>
+      <p>
+        <strong>👴 Client:</strong> Most components are rendered and hydrated
+        after being parsed by your browser. Data fetching only happens in the
+        browser. This attempts to mimic the experience of a traditional React
+        app.
+      </p>
+      <em>
+        *Both versions use a mix of Server and Client Components as well as
+        React Context.
+      </em>
+      <p>
+        Weather data provided by{' '}
+        <a className="underline" href="https://www.weatherapi.com/">
+          weatherapi.com
+        </a>
+        . View the code{' '}
+        <a
+          className="underline"
+          href="https://github.com/gregrickaby/nextjs-app-directory"
+        >
+          on Github
+        </a>
+        .
+      </p>
     </div>
   )
 }
