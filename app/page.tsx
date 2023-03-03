b7dcbd09f64dc9faf06c3dd6c1c62103bda71974
Greg Rickaby <gregrickaby@gmail.com>
2023-03-03 12:24:27 -0600

    don't prefetch
    i want folks to see the speed difference in real-time, not pre-fetched in the background

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,10 @@ export default function Page() {
     <div className="row items-center justify-center">
       <h1>Choose your adventure...</h1>
       <nav>
-        <Link className="button" href="/server/">
+        <Link className="button" href="/server/" prefetch={false}>
           Server Components
         </Link>{' '}
-        <Link className="button" href="/client/">
+        <Link className="button" href="/client/" prefetch={false}>
           Client Components
         </Link>
       </nav>
