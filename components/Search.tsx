e7acc4c9c21413b40ecb4ba3f93b925593b7f375
Greg Rickaby <gregrickaby@gmail.com>
2023-03-03 09:32:36 -0600

    formatting. refactor searchWeather.

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,27 +1,17 @@
-"use client";
+'use client'
 
-import { useRouter } from "next/navigation";
-import slugify from "slugify";
-import { useWeatherContext } from "./WeatherProvider";
+import {useRouter} from 'next/navigation'
+import slugify from 'slugify'
+import {useWeatherContext} from './WeatherProvider'
 
 /**
- * The Search page.
- *
- * Note: because this component uses state, it must be a client component.
- * This also means metadata cannot be generated for this page. If you need
- * to generate metadata for a page that uses state, you must use a server
- * component e.g., app/search/layout.tsx. Additionally, because it uses an NPM
- * package, it must run on the node runtime.
- *
- * @see https://beta.nextjs.org/docs/rendering/server-and-client-components#client-components
+ * The search component.
  */
 export default function Search() {
-  const { location, setLocation } = useWeatherContext();
-  const router = useRouter();
+  const {location, setLocation} = useWeatherContext()
+  const router = useRouter()
 
-  function handleSubmit(e: any) {
-    e.preventDefault();
-
+  function searchWeather() {
     /**
      * Slugify options.
      *
@@ -29,15 +19,15 @@ export default function Search() {
      */
     const slugOptions = {
       lower: true,
-      trim: true,
-    };
+      trim: true
+    }
 
     // Send user to the search results.
-    router.push(`/${slugify(location, slugOptions)}`);
+    router.push(`/${slugify(location, slugOptions)}`)
   }
 
   return (
-    <form className="searchForm">
+    <div className="search">
       <label className="sr-only" htmlFor="location">
         Search Locations
       </label>
@@ -47,7 +37,7 @@ export default function Search() {
         type="text"
         value={location}
       />
-      <button onClick={(e) => handleSubmit(e)}>Search</button>
-    </form>
-  );
+      <button onClick={() => searchWeather()}>Search</button>
+    </div>
+  )
 }
