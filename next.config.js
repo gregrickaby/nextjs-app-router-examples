379b0aae3a95ec470cf335128b153742c90e1b57
Greg Rickaby <gregrickaby@gmail.com>
2023-03-07 11:02:34 -0600

    add remote pattern

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
+    appDir: true
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'nextjswp.dreamhosters.com'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
