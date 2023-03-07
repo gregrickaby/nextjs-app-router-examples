3cc22fadadcbce67ff641f8b6804af2c914aab99
Greg Rickaby <gregrickaby@gmail.com>
2023-03-07 11:03:44 -0600

    add shared header/footer

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -1,3 +1,5 @@
+import Footer from '@/components/shared/Footer'
+import Header from '@/components/shared/Header'
 import {ReactChildren} from '@/lib/types'
 import type {Metadata} from 'next'
 import '../globals.css'
@@ -21,8 +23,10 @@ export default function RootLayout({children}: ReactChildren) {
   return (
     <html lang="en">
       <head />
-      <body>
+      <body className="blog">
+        <Header description="WordPress Blog (Server)" />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   )
