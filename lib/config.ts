07f994a66ea6ba25473fe2bb77a66c5443b9134e
Greg Rickaby <gregrickaby@gmail.com>
2023-03-07 10:28:10 -0600

    add nav

--- lib/config.ts
+++ lib/config.ts
@@ -1,7 +1,25 @@
 const config = {
   siteName: 'Next.js App Directory',
   siteDescription: 'Next.js App Directory Sandbox',
-  siteCredits: '© 2023 Next.js App Directory. All rights reserved.'
+  siteCredits: '© 2023 Next.js App Directory. All rights reserved.',
+  nav: [
+    {
+      name: 'Home',
+      path: '/'
+    },
+    {
+      name: 'Weather (Server)',
+      path: '/wx-server/'
+    },
+    {
+      name: 'Weather (Client)',
+      path: '/wx-client/'
+    },
+    {
+      name: 'Blog',
+      path: '/blog/'
+    }
+  ]
 }
 
 export default config
