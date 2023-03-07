eed4dbb5f02c0084a4905ce7278e5a43dd23a132
Greg Rickaby <gregrickaby@gmail.com>
2023-03-07 11:03:35 -0600

    use dynamic metadata

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,6 +1,25 @@
 import {getPost} from '@/lib/functions'
+import {Metadata} from 'next'
 
 export const runtime = 'experimental-edge'
+
+/**
+ * Generate dynamic metadadta.
+ *
+ * @see https://beta.nextjs.org/docs/guides/seo#dynamic-metadata
+ */
+export async function generateMetadata({
+  params
+}: {
+  params: {slug: string}
+}): Promise<Metadata> {
+  // Get the blog post.
+  const {post} = await getPost(params.slug)
+
+  return {
+    title: post.title
+  }
+}
 
 /**
  * Generate posts for static generation.
@@ -32,7 +51,7 @@ export default async function Page({params}: {params: {slug: string}}) {
   }
 
   return (
-    <article className="max-w-3xl">
+    <article>
       <h2 dangerouslySetInnerHTML={{__html: post.title}} />
       <div dangerouslySetInnerHTML={{__html: post.content}} />
     </article>
