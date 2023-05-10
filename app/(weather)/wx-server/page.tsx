b058a08b4f357768255a2305c1ba8a64ea227d0d
Greg Rickaby <gregrickaby@gmail.com>
2023-05-10 10:25:32 -0500

    fix wx-server homepage

--- app/(weather)/wx-server/page.tsx
+++ app/(weather)/wx-server/page.tsx
@@ -1,39 +1,17 @@
 import DisplayWeather from '@/components/wx-server/DisplayWeather'
 import {getForecast} from '@/lib/functions'
-import {LocationPageProps} from '@/lib/types'
-import {Metadata} from 'next'
 import {notFound} from 'next/navigation'
 
 export const runtime = 'edge'
 
 /**
- * Generate dynamic metadadta.
+ * The weather (server) homepage.
  *
- * @see https://beta.nextjs.org/docs/guides/seo#dynamic-metadata
+ * @see https://beta.nextjs.org/docs/routing/pages-and-layouts
  */
-export async function generateMetadata({
-  params
-}: LocationPageProps): Promise<Metadata> {
-  // Get the forecast for the location.
-  const forecast = await getForecast(params.location)
-
-  return {
-    title: `Search Results - ${forecast.address}`,
-    description: `The local forecast for ${forecast.address}`
-  }
-}
-
-/**
- * The weather (server) search results page.
- *
- * This page is a dynamic segment, so it will display the forecast
- * for any location provided in the URL.
- *
- * @see https://beta.nextjs.org/docs/routing/defining-routes#dynamic-segments
- */
-export default async function SearchResults({params}: LocationPageProps) {
-  // Get the forecast for the location.
-  const {weather} = await getForecast(params.location)
+export default async function WxServerHome() {
+  // Get the weather forecast.
+  const {weather} = await getForecast('Enterprise, AL')
 
   // No forecast? Bail...
   if (!weather) {
