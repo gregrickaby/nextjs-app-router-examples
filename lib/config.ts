f67134631d187a3f95bc36995276c89708d0e350
Greg Rickaby <gregrickaby@gmail.com>
2023-06-27 15:04:39 -0500

    update footer

--- lib/config.ts
+++ lib/config.ts
@@ -1,9 +1,10 @@
 const config = {
   siteName: 'Next.js App Router Examples',
   siteDescription: 'Examples for the new app router in Next 13',
-  siteCredits: '© 2023 Greg Rickaby - Next.js App Router Examples',
   siteUrl: 'https://nextjs-app-router-examples.vercel.app',
   githubUrl: 'https://github.com/gregrickaby/nextjs-app-router-examples',
+  author: 'Greg Rickaby',
+  authorUrl: 'https://gregrickaby.com',
   nav: [
     {
       name: 'Home',
