380818d92d337b650d29ea9da956a3d62691f4e2
Greg Rickaby <gregrickaby@gmail.com>
2023-06-28 10:54:34 -0500

    refactor. add link.

--- app/blog/not-found.tsx
+++ app/blog/not-found.tsx
@@ -1,9 +1,12 @@
-const NotFound = () => {
+/**
+ * Not Found component.
+ *
+ * @see https://nextjs.org/docs/app/api-reference/file-conventions/not-found
+ */
+export default function NotFound() {
   return (
     <div className="flex min-h-[5rem] items-center justify-center text-xl font-bold">
-      ⚠️ Post not found.
+      ⚠️ Post not found!
     </div>
   )
 }
-
-export default NotFound
